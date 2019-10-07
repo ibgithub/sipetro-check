3cd2272eb0128925bfade095641e43d55a8e6ced
imam baihaqi <imam.baihaqi1999@gmail.com>
2019-10-07 16:03:26 +0700

    design enhancement

--- src/app/srf/srf.component.ts
+++ src/app/srf/srf.component.ts
@@ -22,7 +22,31 @@ const ELEMENT_DATA: Srf[] = [
   {date: '20190927', serverName: 'DC Colo Primary', fileName: '20190927.1.srf.gz', fileSize: '76 MB', lastModifiedDate: '27/09/2019', lastModifiedTime: '16:35:01'},
   {date: '20190928', serverName: 'DC Colo Primary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
   {date: '20190929', serverName: 'DC Colo Secondary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
-  {date: '20190930', serverName: 'DC Colo Secondary', fileName: '20190930.1.srf.gz', fileSize: '84 MB', lastModifiedDate: '30/09/2019', lastModifiedTime: '16:35:01'}
+  {date: '20190930', serverName: 'DC Colo Secondary', fileName: '20190930.1.srf.gz', fileSize: '84 MB', lastModifiedDate: '30/09/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191001', serverName: 'Koperbi Primary', fileName: '20191001.1.srf.gz', fileSize: '88 MB', lastModifiedDate: '01/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191002', serverName: 'Koperbi Primary', fileName: '20191002.1.srf.gz', fileSize: '100 MB', lastModifiedDate: '02/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191003', serverName: 'Koperbi Secondary', fileName: '20191003.1.srf.gz', fileSize: '97 MB', lastModifiedDate: '03/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191004', serverName: 'Koperbi Secondary', fileName: '20191004.1.srf.gz', fileSize: '88 MB', lastModifiedDate: '04/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191005', serverName: 'DC Colo Primary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
+  {date: '20191006', serverName: 'DC Colo Primary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
+  {date: '20191007', serverName: 'DC Colo Secondary', fileName: '20191007.1.srf.gz', fileSize: '257 MB', lastModifiedDate: '07/10/2019', lastModifiedTime: '14:41:14'},
+  {date: '20190926', serverName: 'DC Colo Secondary', fileName: '20190926.1.srf.gz', fileSize: '96 MB', lastModifiedDate: '26/09/2019', lastModifiedTime: '16:35:01'},
+  {date: '20180604', serverName: 'Koperbi Primary', fileName: '20180604.1.srf.gz', fileSize: '30 MB', lastModifiedDate: '04/06/2018', lastModifiedTime: '16:35:01'},
+  {date: '20190913', serverName: 'Koperbi Primary', fileName: 'NOT_EXIST', fileSize: 'NOT_EXIST', lastModifiedDate: 'NOT_EXIST', lastModifiedTime: 'NOT_EXIST'},
+  {date: '20190925', serverName: 'Koperbi Secondary', fileName: '20190925.1.srf.gz', fileSize: '88 MB', lastModifiedDate: '25/09/2019', lastModifiedTime: '16:35:01'},
+  {date: '20190926', serverName: 'Koperbi Secondary', fileName: '20190926.1.srf.gz', fileSize: '96 MB', lastModifiedDate: '26/09/2019', lastModifiedTime: '16:35:01'},
+  {date: '20190927', serverName: 'DC Colo Primary', fileName: '20190927.1.srf.gz', fileSize: '76 MB', lastModifiedDate: '27/09/2019', lastModifiedTime: '16:35:01'},
+  {date: '20190928', serverName: 'DC Colo Primary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
+  {date: '20190929', serverName: 'DC Colo Secondary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
+  {date: '20190930', serverName: 'DC Colo Secondary', fileName: '20190930.1.srf.gz', fileSize: '84 MB', lastModifiedDate: '30/09/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191001', serverName: 'Koperbi Primary', fileName: '20191001.1.srf.gz', fileSize: '88 MB', lastModifiedDate: '01/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191002', serverName: 'Koperbi Primary', fileName: '20191002.1.srf.gz', fileSize: '100 MB', lastModifiedDate: '02/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191003', serverName: 'Koperbi Secondary', fileName: '20191003.1.srf.gz', fileSize: '97 MB', lastModifiedDate: '03/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191004', serverName: 'Koperbi Secondary', fileName: '20191004.1.srf.gz', fileSize: '88 MB', lastModifiedDate: '04/10/2019', lastModifiedTime: '16:35:01'},
+  {date: '20191005', serverName: 'DC Colo Primary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
+  {date: '20191006', serverName: 'DC Colo Primary', fileName: 'HOLIDAY', fileSize: 'HOLIDAY', lastModifiedDate: 'HOLIDAY', lastModifiedTime: 'HOLIDAY'},
+  {date: '20191007', serverName: 'DC Colo Secondary', fileName: '20191007.1.srf.gz', fileSize: '257 MB', lastModifiedDate: '07/10/2019', lastModifiedTime: '14:41:14'},
+  {date: '20190926', serverName: 'DC Colo Secondary', fileName: '20190926.1.srf.gz', fileSize: '96 MB', lastModifiedDate: '26/09/2019', lastModifiedTime: '16:35:01'}
 ];
 
 @Component({
